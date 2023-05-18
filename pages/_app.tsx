406d6e773aecaa652e19a2bc168393f4b7e088c4
esaia <esaiagafrindashvili@gmail.com>
2023-05-18 10:51:14 +0400

    initial

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
+    <div>
+      <Head>
+        <title>Epic movie</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
+}
