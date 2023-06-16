b32e2ae13783006482928fc1b2bc0b53e988b382
esaia <esaiagafrindashvili@gmail.com>
2023-06-16 13:36:42 +0400

    fix: create absolute importes

---
+++ Hooks/index.ts
@@ -0,0 +1,6 @@
+export { default as useHome } from "./useHome";
+export { default as useGoogleAuth } from "./useGoogleAuth";
+export { default as useLanding } from "./useLanding";
+export { default as useMovie } from "./useMovie";
+export { default as useMovies } from "./useMovies";
+export { default as useProfile } from "./useProfile";
