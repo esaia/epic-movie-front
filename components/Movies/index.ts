efe33a1736e345c4891d000229d9e9b8778c9138
esaia <esaia@gmail.com>
2023-07-07 14:24:05 +0400

    make better folder structure

---
+++ components/Movies/index.ts
@@ -0,0 +1,5 @@
+export * from "./CreateMovieModal";
+export * from "./EditMovieModal";
+export * from "./MoviePost";
+export * from "./MovieWrapper";
+export * from "./SingleQuote";
---
+++ components/Quotes/index.ts
@@ -0,0 +1,5 @@
+export * from "./AddQuote";
+export * from "./CreateQuoteModal";
+export * from "./EditQuote";
+export * from "./ViewQuote";
+export * from "./LandingQuote";
